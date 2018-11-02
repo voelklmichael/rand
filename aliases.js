a523eb501800cfc7e4586bc696f58b99d62fd30b
Travis CI User <travis@example.org>
2018-11-02 14:42:49 +0000

    Documentation for rust-random/rand@be9c864

---
+++ aliases.js
@@ -0,0 +1,8 @@
+var ALIASES = {};
+ALIASES['rand'] = {};
+ALIASES['rand_chacha'] = {};
+ALIASES['rand_core'] = {};
+ALIASES['rand_hc'] = {};
+ALIASES['rand_isaac'] = {};
+ALIASES['rand_pcg'] = {};
+ALIASES['rand_xorshift'] = {};
